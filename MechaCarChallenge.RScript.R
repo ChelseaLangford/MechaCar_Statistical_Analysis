dfe19195193bfe4c1f6a24ec6e35ce3445c0dd25
Chelsea Langford <chelseallangford@gmail.com>
2021-09-30 01:30:32 -0400

    adding challenge code

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -12,3 +12,14 @@ suspension <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors
 total_summary <- suspension %>% summarize(mean(PSI), median(PSI), Variance=var(PSI), SD=sd(PSI))
 
 lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(mean(PSI), median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+t.test(suspension$PSI, mu=1500) #t-test for all manufacturing lots
+
+Lot1_sample = subset(suspension, Manufacturing_Lot == "Lot1") #t-test for Lot 1
+t.test(Lot1_sample$PSI, mu=1500)
+
+Lot2_sample = subset(suspension, Manufacturing_Lot == "Lot2") #t-test for Lot 2
+t.test(Lot2_sample$PSI, mu=1500)
+
+Lot3_sample = subset(suspension, Manufacturing_Lot == "Lot3") #t-test for Lot 3
+t.test(Lot3_sample$PSI, mu=1500)
